0834c725d32812585af69a59fa5e0643b1e73120
Sunil-Java <33261956+Sunil-Java@users.noreply.github.com>
2021-03-15 11:50:46 +0530

    Update Test.java

--- Test/src/com/Test.java
+++ Test/src/com/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello Testing git");
 		
-		System.out.println("Chanxgew in file. testing stash pop");
+		System.out.println("Changes in file. testing stash pop");
 		
 		System.out.println("Check git Commit test");
 	}
